a1032a6b7cb158936e2174a176a574be2dc5120a
Venkat Motupalli <vrmotupalli@gmail.com>
2015-09-26 16:28:19 -0400

    HW2 submit
    get_bus_info.py and show_bus_location.py

---
+++ get_bus_info.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 26 15:54:19 2015
+
+@author: Mots
+
+Full URL with my key and a bus so I can cut and paste...
+
+http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key=74c47369-4c4c-4c30-bb58-8ac0e2642d9b&Vehicle-MonitoringDetailLevel=calls&LineRef=B52
+"""
+import json
+import sys
+#import urllib2
+import urllib.request as urllib2
+import csv
+
+if __name__=='__main__':
+    
+    #input variables    
+    key = sys.argv[1]  
+    busID = sys.argv[2]    
+    
+    url = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s' % (key, busID)
+    
+    request = urllib2.urlopen(url)
+    data = json.loads(request.read())
+    
+    buses = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+    
+    #Py3 syntax: with open(filename, 'w', newline='') as csvFile:    
+    
+    with open(sys.argv[3], 'w', newLine='') as csvFile:
+        writer = csv.writer(csvFile)
+        headers = ['Latitude','Longitude','Stop Name','Stop Status']
+        writer.writerow(headers)
+        
+        for i in buses:
+            lat  = i['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+            long  = i['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+            if i['MonitoredVehicleJourney']['OnwardCalls'] == {}:
+                stop = 'N/A'
+                status = 'N/A'
+            else:
+                StopName = i['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName']
+                StopStatus = i['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']
+            writer.writerow([lat,long,stop,status])
---
+++ show_bus_location.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 26 15:14:47 2015
+
+@author: Mots
+
+
+Full URL with my key and a bus so I can cut and paste...
+
+http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key=74c47369-4c4c-4c30-bb58-8ac0e2642d9b&Vehicle-MonitoringDetailLevel=calls&LineRef=B52
+"""
+
+import json
+import sys
+#import urllib2
+import urllib.request as urllib2
+
+if __name__=='__main__':
+
+    # input variables    
+    key = sys.argv[1]  
+    busID = sys.argv[2]  
+     
+    url = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s' % (key, busID)
+    
+    request = urllib2.urlopen(url)
+    data = json.loads(request.read())
+    
+    """    
+    Path is from CodeBeautify's JSON viewer www.codebeautify.com/jsonviewer
+    I looked at some class repos to figure out the syntax for calling a path
+    """
+    buses = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+    
+    print ("Bus Line : %s" % busID)      
+    print ("Number of Active Buses : %d" % (len(buses)))
+    for i in range(len(buses)):
+        locations = buses[i]['MonitoredVehicleJourney']["VehicleLocation"]
+        lat = locations['Latitude']
+        long = locations['Longitude']
+        print ("Bus %i is at latitude %f and longitude %f" % (i, lat, long))
